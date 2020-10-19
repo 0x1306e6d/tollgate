6c8ad3edb1c673f2c99bb66e3b562ce616b1fe19
Gihwan Kim <ghkim3221@gmail.com>
2020-10-20 00:40:03 +0900

    Load configuration using lightbend/config (#49)

--- standalone/src/main/java/dev/gihwan/tollgate/standalone/Main.java
+++ standalone/src/main/java/dev/gihwan/tollgate/standalone/Main.java
@@ -26,13 +26,27 @@ package dev.gihwan.tollgate.standalone;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.annotation.Nullable;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import com.typesafe.config.ConfigObject;
+
+import com.linecorp.armeria.common.HttpMethod;
+
 import dev.gihwan.tollgate.core.Tollgate;
+import dev.gihwan.tollgate.core.TollgateBuilder;
+import dev.gihwan.tollgate.core.endpoint.EndpointConfig;
+import dev.gihwan.tollgate.core.service.Authority;
+import dev.gihwan.tollgate.core.service.ServiceConfig;
+import dev.gihwan.tollgate.core.upstream.UpstreamConfig;
+import dev.gihwan.tollgate.core.upstream.UpstreamEndpointConfig;
 
 public final class Main {
 
@@ -47,14 +61,58 @@ public final class Main {
         startAt = Instant.now();
         logger.info("Starting the Tollgate standalone at {}.", startAt);
 
-        tollgate = Tollgate.builder()
-                           .port(8080)
-                           .build();
+        final Config config = ConfigFactory.load();
+        logger.debug("Loaded configuration: {}", config);
+
+        final TollgateBuilder builder = Tollgate.builder()
+                                                .port(config.getInt("tollgate.port"))
+                                                .healthCheckPath(config.getString("tollgate.healthCheckPath"));
+
+        config.getObjectList("tollgate.endpoints")
+              .stream()
+              .map(ConfigObject::toConfig)
+              .forEach(endpointConfig -> buildEndpoint(builder, endpointConfig));
+
+        tollgate = builder.build();
         tollgate.start();
 
         assert startAt != null;
         logger.info("The Tollgate standalone is started. ({} used to start)",
                     Duration.between(startAt, Instant.now()));
+    }
+
+    private void buildEndpoint(TollgateBuilder builder, Config endpointConfig) {
+        final Config upstreamServiceConfig = endpointConfig.getObject("upstream.service").toConfig();
+        final ServiceConfig service = buildService(upstreamServiceConfig);
+
+        final Config upstreamEndpointConfig = endpointConfig.getObject("upstream.endpoint").toConfig();
+        final UpstreamEndpointConfig upstreamEndpoint = buildUpstreamEndpoint(upstreamEndpointConfig);
+
+        builder.endpoint(EndpointConfig.of(endpointConfig.getEnum(HttpMethod.class, "method"),
+                                           endpointConfig.getString("path"),
+                                           UpstreamConfig.of(service, upstreamEndpoint)));
+    }
+
+    private ServiceConfig buildService(Config config) {
+        if (config.hasPath("uri")) {
+            return ServiceConfig.of(config.getString("uri"));
+        } else {
+            List<Authority> authorities = config.getObjectList("authorities")
+                                                .stream()
+                                                .map(ConfigObject::toConfig)
+                                                .map(authorityConfig -> {
+                                                    final String host = authorityConfig.getString("host");
+                                                    final int port = authorityConfig.getInt("port");
+                                                    return Authority.of(host, port);
+                                                })
+                                                .collect(Collectors.toUnmodifiableList());
+            return ServiceConfig.of(config.getString("scheme"), authorities);
+        }
+    }
+
+    private UpstreamEndpointConfig buildUpstreamEndpoint(Config config) {
+        return UpstreamEndpointConfig.of(config.getEnum(HttpMethod.class, "method"),
+                                         config.getString("path"));
     }
 
     private void stop() {
