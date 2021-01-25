fa49453353eaeda952beda43e1476cbcae74120b
Gihwan Kim <ghkim3221@gmail.com>
2021-01-25 23:57:23 +0900

    Enable to customize building gateway from HOCON (#91)

---
+++ hocon/src/main/java/dev/gihwan/tollgate/hocon/HoconGatewayConfigurator.java
@@ -0,0 +1,49 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 - 2021 Gihwan Kim
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package dev.gihwan.tollgate.hocon;
+
+import com.typesafe.config.Config;
+
+import dev.gihwan.tollgate.gateway.Gateway;
+import dev.gihwan.tollgate.gateway.GatewayBuilder;
+
+/**
+ * A {@link FunctionalInterface} for configuring a {@link Gateway} using a {@link Config}.
+ */
+@FunctionalInterface
+public interface HoconGatewayConfigurator {
+
+    /**
+     * Returns the default implementation of {@link HoconGatewayConfigurator}.
+     */
+    static HoconGatewayConfigurator ofDefault() {
+        return DefaultHoconGatewayConfigurator.INSTANCE;
+    }
+
+    /**
+     * Configures a {@link Gateway} using the given {@link Config}.
+     */
+    void configure(GatewayBuilder builder, Config config);
+}
