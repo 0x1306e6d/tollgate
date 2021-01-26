e367f8b64f16dae752884529d5dc27924c461007
Gihwan Kim <ghkim3221@gmail.com>
2021-01-26 23:02:43 +0900

    Check argument instead of state when build in builder

--- remapping/src/main/java/dev/gihwan/tollgate/remapping/RemappingRequestUpstreamBuilder.java
+++ remapping/src/main/java/dev/gihwan/tollgate/remapping/RemappingRequestUpstreamBuilder.java
@@ -24,6 +24,7 @@
 
 package dev.gihwan.tollgate.remapping;
 
+import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkState;
 import static java.util.Objects.requireNonNull;
 
@@ -80,7 +81,7 @@ public final class RemappingRequestUpstreamBuilder {
      * Builds a new {@link RemappingRequestUpstream} based on the properties of this builder.
      */
     public RemappingRequestUpstream build(Upstream delegate) {
-        checkState(strategy != null, "Must set at lease one remapping strategy");
+        checkArgument(strategy != null, "Must set at lease one remapping strategy");
         return new RemappingRequestUpstream(delegate, strategy);
     }
 }
