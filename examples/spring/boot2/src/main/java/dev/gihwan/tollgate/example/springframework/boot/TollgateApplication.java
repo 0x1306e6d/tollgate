8754f626a37e4b06e767a1c82d191ad570e2999c
Gihwan Kim <ghkim3221@gmail.com>
2021-12-19 17:00:06 +0900

    Fix package

--- examples/spring/boot2/src/main/java/dev/gihwan/tollgate/example/springframework/boot/TollgateApplication.java
+++ examples/spring/boot2/src/main/java/dev/gihwan/tollgate/example/springframework/boot/TollgateApplication.java
@@ -22,7 +22,7 @@
  * SOFTWARE.
  */
 
-package boot;
+package dev.gihwan.tollgate.example.springframework.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
